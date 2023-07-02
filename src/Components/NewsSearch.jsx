1374760ca714e88eaec523a56c641a82616d26c8
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-07-02 09:54:52 +0400

    added news

--- src/Components/NewsSearch.jsx
+++ src/Components/NewsSearch.jsx
@@ -1,39 +1,76 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-const NewsSearch = () => {
-  const [result, setResult] = useState([]);
+const API_KEY = "pub_25576383d108bfd252758ab16fe54450fd226";
+const SEARCH_QUERY = "hadise";
 
-  const fetchNews = async () => {
-    try {
-      const response = await fetch(
-        `https://newsdata.io/api/1/news?apikey=pub_25576383d108bfd252758ab16fe54450fd226&q=pizza`
-      );
-      const data = await response.json();
-      if (data.response && data.response.results) {
-        setResult(data.response.results);
-        console.log(data.response.results);
-      } else {
-        setResult([]); // Set an empty array if 'results' is undefined
-      }
-    } catch (error) {
-      console.error('Error fetching news:', error);
-      setResult([]); // Set an empty array in case of error
-    }
-  };
+const App = () => {
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    fetchNews();
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://newsdata.io/api/1/news?apikey=${API_KEY}&q=${encodeURIComponent(
+            SEARCH_QUERY
+          )}`
+        );
+        const data = await response.json();
+        setResults(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div>
-      {result.map((each) => (
-        <div >
-          <h3>{each.title}</h3>
-        </div>
-      ))}
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold mb-4">Results:</h1>
+      <ul className="grid gap-6">
+        {results.map((result, index) => (
+          <li key={index} className="grid grid-cols-2 gap-4">
+            <div className="col-span-1">
+              <img
+                src={result.image_url}
+                className="w-full h-auto"
+                alt="No picture available"
+              />
+            </div>
+            <div className="col-span-1">
+              <h2 className="text-xl font-bold mb-2">{result.topic}</h2>
+              <p className="text-gray-700 mb-4">
+                {result.description.split(" ").slice(0, 50).join(" ")}
+                {result.description.split(" ").length > 50 && "... Read more"}
+              </p>
+              <div className="flex flex-col">
+                <p>
+                  <span className="font-bold">Date: </span>
+                  {result.pubDate}
+                </p>
+                <p>
+                  <span className="font-bold">Country: </span>
+                  {result.country}
+                </p>
+                <p>
+                  <span className="font-bold">Language: </span>
+                  {result.language}
+                </p>
+                <p>
+                  <span className="font-bold">Publisher: </span>
+                  {result.creator}
+                </p>
+                <p>
+                  <span className="font-bold">Category: </span>
+                  {result.category}
+                </p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default NewsSearch;
+export default App;
